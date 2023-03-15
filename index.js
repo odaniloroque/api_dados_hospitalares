0cd3d67896e19b17e92d3d13a74f4ec8ca896563
Danilo Roque <dsr.proj3ct@gmail.com>
2023-03-15 16:53:12 -0300

    adicionando rotas de teste de coneção

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 
 require('dotenv').config();
 
-
 app.use(express.json());
 app.use(morgan('tiny'));
+
 
 const config = {
   user: process.env.DB_USER,
@@ -16,20 +16,21 @@ const config = {
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
   options: {
-    encrypt: true, // Se você estiver usando o Azure
+    encrypt: false, // Se você estiver usando o Azure
     trustServerCertificate: true // Altere esta opção se estiver usando SSL
   }
 };
 
 const pool = new mssql.ConnectionPool(config);
 
-pool.connect(err => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Conectado ao MSSQL');
-  }
-});
+// pool.connect(err => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log('Conectado ao MSSQL');
+//   }
+// });
+
 
 app.get('/', (req, res) => {
   pool.request().query('SELECT * FROM paciente', (err, result) => {
@@ -40,6 +41,21 @@ app.get('/', (req, res) => {
       res.send(result.recordset);
     }
   });
+});
+
+// Route for testing the database connection
+app.get('/test-connection', async (req, res) => {
+  try {
+    await mssql.connect(config);
+    const result = await sql.query`SELECT 1 AS Result`;
+    console.dir(result);
+    res.send('Database connection successful!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to connect to database');
+  } finally {
+    await mssql.close();
+  }
 });
 
 app.listen(3000, () => {
